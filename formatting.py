a13cff4ff4e8f8e7e33561e802408e3b80af3004
NavinHajari <navinsingh_hajari@yahoo.co.in>
2026-06-09 09:04:26 +0530

    new files added

---
+++ formatting.py
@@ -0,0 +1,12 @@
+for i in range(1, 13):
+    #print("No. {0} squared is {1} and cubed is {2}" .format(i, i ** 2, i ** 3))
+    print("No. {0:2} squared is {1:3} and cubed is {2:4}" .format(i, i ** 2, i ** 3))
+
+print()
+
+for i in range(1, 13):
+    a = i
+    b = i ** 2
+    c = i ** 3 
+    print(f"No. {a:2} squared is {b:<3} and cubed is {c:^4}")
+
---
+++ sequence_operators.py
@@ -0,0 +1,15 @@
+string1 = "he's"
+strings = "probably "
+strings3 = "for the "
+strings4 = "fjords"
+
+print(string1 + strings + strings3 + strings4)
+
+print("hello " * 5)
+
+print("hello " * (5+4))
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
